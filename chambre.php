1bd2c6da6e6c7e2cc6b0d7a3a7b2ff2fc9e0e03f
Damdam <francis.munabeno@epitech.eu>
2021-03-11 14:27:53 +0100

    premier envoi

---
+++ chambre.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style/style.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Srisakdi:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
+
+
+
+
+<div class="premierePartie">
+  <h1>Chaque chambre est unique</h1>
+  <p>Vous êtes ici chez vous</p>
+  <div class="photoCarré">
+    <div>
+      <img src="img/chambreBlancMini.jpg" alt="photo en miniature d'une chambre avec une tapisserie de map monde sur le mur couleur blanc et or">
+      <img class="boutonPlus" src="img/add.png" alt="un bouton blanc d'un plus">
+    </div>
+    <div>
+      <img src="img/chambreBoiserie2Mini.jpg" alt="Chambre avec des boiseries">
+      <img class="boutonPlus"  src="img/add.png" alt="un bouton blanc d'un plus">
+    </div>
+    <div>
+      <img src="img/chambreNoirMini.jpg" alt="Chambre en marbre de couleur noir">
+      <img class="boutonPlus"  src="img/add.png" alt="un bouton blanc d'un plus">
+    </div>
+  </div>
+</div>
+
+<div class="deuxiemePartie">
+  <div class="imageChambre">
+    <div class="imgChambre1"></div>
+  </div>
+  <div class="cadreBleu">
+    <ul>
+      <li>
+      <img src="img/air-conditioner.png" alt="petit logo représentant une particule de neige">  
+      Climatisation</li>
+      <li>
+      <img src="img/safe.png" alt="petit logo représentant un cadenas">  
+      Coffre fort</li>
+      <li>
+      <img src="img/cocktail.png" alt="petit verre de cocktail">  
+      Mini Bar</li>
+      <li>
+      <img src="img/tray.png" alt="un plat chaud couvert">  
+      Plateau courtoisie</li>
+      <li>
+      <img src="img/bathrobe.png" alt="un peignoir">  
+      Peignoirs</li>
+    </ul>
+      <i class="fas fa-plus-circle fa-3x"></i>
+  </div>
+  <div class="texteChambre">
+    <div class="widthTexte">
+      <h2>Chambre Map Monde</h2>
+      <p class="premierTexte">D’une superficie de <span>15m²</span>, cette chambre séduit par son cadre «<span>cosy</span>».</p>
+      <p class="deuxiemeTexte">Doté d’un <span>lit double</span> (180X200 cm), l’espace y est particulièrement bien utilisé, plusieurs rangements dont une <span>armoire</span> seront à votre disposition. Un <span>plateau de courtoisie</span> et également mis à votre service.</p>
+      <button>Réserver</button>
+    </div>
+  </div>
+</div>
+
+<div class="deuxiemePartie">
+  <div class="texteChambre">
+    <div class="widthTexte">
+      <h2>Chambre Map Monde</h2>
+      <p class="premierTexte">D’une superficie de <span>15m²</span>, cette chambre séduit par son cadre «<span>cosy</span>».</p>
+      <p class="deuxiemeTexte">Doté d’un <span>lit double</span> (180X200 cm), l’espace y est particulièrement bien utilisé, plusieurs rangements dont une <span>armoire</span> seront à votre disposition. Un <span>plateau de courtoisie</span> et également mis à votre service.</p>
+      <button>Réserver</button>
+    </div>
+  </div>
+  <div class="imageChambre">
+    <div class="imgChambre2"></div>
+  </div>
+</div>
+
+<div class="deuxiemePartie">
+  <div class="imageChambre">
+    <div class="imgChambre3"></div>
+  </div>
+  <div class="texteChambre">
+    <div class="widthTexte">
+      <h2>Chambre Map Monde</h2>
+      <p class="premierTexte">D’une superficie de <span>15m²</span>, cette chambre séduit par son cadre «<span>cosy</span>».</p>
+      <p class="deuxiemeTexte">Doté d’un <span>lit double</span> (180X200 cm), l’espace y est particulièrement bien utilisé, plusieurs rangements dont une <span>armoire</span> seront à votre disposition. Un <span>plateau de courtoisie</span> et également mis à votre service.</p>
+      <button>Réserver</button>
+    </div>
+  </div>
+</div>
