aed4ee4a1eef9b5895c15fe166d1e943309e2079
Gaëlle <gaelle.wf3@gmail.com>
2021-03-26 11:29:25 +0100

    lien footer

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -3,17 +3,20 @@
 
 
 <div class="logo">
-    <img  class="booking" src="img/booking-logo-fond-blanc.png">
-
-    <img class="ig" src="img/ig.png">
+     
+   <a href="https://www.booking.com/index.fr.html" target="_blank"><img class="booking" src="img/booking-logo-fond-blanc.png"></a>
+  
+   <a href="https://www.instagram.com/?hl=fr" target="_blank"><img class="ig" src="img/ig.png"></a>
+ 
 </div>
 
 
  <div class="copyright">
      <p>©Copyright HotelSuccess2021</p>
 
-     <p>Design / Developpement TestiBuzz</p>
+     <p class="buzz"><a href="http://testibuzz.com/" target="_blank">Design / Developpement TestiBuzz</p>
 
-    <p><a href="mentions.php"> Mentions légales</a></p>
+    <p class="mentions"><a href="mentions.php"> Mentions légales</a></p>
+</div>
 </footer>
 
